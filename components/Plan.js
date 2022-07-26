7be4e8d0e985b8700f94f250e27309ed6c4dbcc8
Nathan Igdaloff <louis.nathan@gmail.com>
2022-07-26 11:23:27 -0700

    get today's date

--- components/Plan.js
+++ components/Plan.js
@@ -26,10 +26,19 @@ export default function Plan() {
     window.localStorage.setItem('values', JSON.stringify(values))
   }, [values]);
 
+  // Get today's date
+  const d = new Date();
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const months = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"];
+  const date = String(d.getDate()).padStart(2, '0');
+  const year = d.getFullYear()
+  const today = days[d.getDay()] + ", " + months[d.getMonth()] + " " + date + ", " + year;
+  
   return ( 
     <>
       <h1>What do you want to do today?</h1>
-      <h2>Thu Jul 21 2022</h2>      
+      <h2>{today}</h2>      
       <form className={styles.planForm}>
         <ul>
           <li className={styles.hourListItem}>
